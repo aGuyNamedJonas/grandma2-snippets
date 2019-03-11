ffb6add12fb98c5307993be0ef324db26e9f0c22
Jonas <jonas.peeck@googlemail.com>
2019-03-11 19:47:22 +0100

    Add ma2onpc backup powershell script

---
+++ onpc-backup-script/CreateMA2ShowFolderBackup.ps1
@@ -0,0 +1,36 @@
+﻿<#
+        Original Version by: Philipp Börner
+        Source: https://freshblog.azurewebsites.net/2016/03/28/backup-you-work-with-powershell/
+
+        Adapted by @aGuyNamedJonas for MA2 onPC
+
+        ---------------------------------------------
+        SUBSCRIBE      >>> youtube.com/aGuyNamedJonas
+        QA & COMMUNITY >>> fb.com/aGuyNamedJonas
+        GET IN TOUCH   >>> hi@aGuyNamedJonas.com
+        ---------------------------------------------
+
+        HOW TO USE:
+        - Make sure $source points to your PC's showfolder
+        - Change $dest to point to your harddrive / cloud storage folder (=backup folder)
+
+#>
+ 
+#Define source and destination folders
+$source = 'C:\ProgramData\MA Lighting Technologies\grandma'
+$dest = 'F:\Google Drive\2019\Showfolder Backups\'
+ 
+#Getting the current date
+$date = Get-Date -UFormat _%d_%m_%Y
+ 
+#Building ZIP file name and complete backup path
+$ZipFile = 'MA2ShowFolderBackup'+$date+'.zip'
+$BackupPath = $dest+$ZipFile
+ 
+#creating GUID if ZIP archive already exist, to rename the pre existing file
+$GUID = [GUID]::NewGuid().ToString()
+$doubleBackup = $ZipFile+$GUID
+if (Test-Path $BackupPath) {Rename-Item $BackupPath -NewName $doubleBackup}
+ 
+#Creating ZIP archive
+Get-Item -Path $source | Compress-Archive -DestinationPath $BackupPath -CompressionLevel Optimal -Verbose
---
+++ onpc-backup-script/README.md
@@ -0,0 +1,10 @@
+# MA2 onPC Show Folder Backup Script
+This Windows Powershell script will allow you to create backups of your entire showfolder on an external drive or your cloud storage (e.g. Google Drive, Dropbox, etc.).
+
+This closes the gap that the missing support for FTP backups in onPC systems left us with.
+
+# How to use
+Adapt `$source` and `$dest` to your needs, and then execute the script by double clicking on it.
+
+# Trouble shooting ("executon of scripts is disabled")
+In case you get the error message `“execution of scripts is disabled on this system.”`, [try this solution from StackOverflow](https://stackoverflow.com/questions/4037939/powershell-says-execution-of-scripts-is-disabled-on-this-system).
